c48548887e94dbfcf1da1fbc355848a415b64d30
rch850 <rich850@gmail.com>
2019-08-12 15:51:29 +0900

    👍 Add local camera preview

--- packages/webrtc-pinp/src/app/app.component.ts
+++ packages/webrtc-pinp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webrtc-pinp';
+
+  @ViewChild('localVideo') localVideo: ElementRef<HTMLVideoElement>
+
+  onSetupCameraClick (): void {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(mediaStreams => {
+      this.localVideo.nativeElement.srcObject = mediaStreams
+    })
+  }
 }
