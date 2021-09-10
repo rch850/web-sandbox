a9820445dd8638602f1e92e506d9ef46bfbcaaca
rch850 <rich850@gmail.com>
2021-09-10 23:57:01 +0900

    👍 excaliburjs-demo: Erase cells

--- packages/excaliburjs-demo/index.ts
+++ packages/excaliburjs-demo/index.ts
@@ -1,4 +1,4 @@
-import { Actor, CollisionType, Color, DisplayMode, Engine, vec } from 'excalibur'
+import { Actor, Color, Engine } from 'excalibur'
 
 const SIZE = 600
 const COLORS = [Color.Green, Color.Red, Color.Blue, Color.Yellow]
@@ -56,8 +56,13 @@ for (let y = 0; y < 5; y++) {
       dragY = thatY
     })
 
-    cell.on('pointerdragend', () => {
-      console.log('pointerdragend')
+    cell.on('pointerdragend', (ev) => {
+      console.log('pointerdragend', ev)
+      // Xperia 1 II (Android 11) だと Left と Unknown で2回イベントが来たので、
+      // Left だけを採用します。
+      if (ev.button === "Left") {
+        eraseCells(cells)
+      }
     })
 
     cells[y][x] = cell
@@ -67,3 +72,60 @@ for (let y = 0; y < 5; y++) {
 
 // Start the engine to begin the game.
 game.start()
+
+function eraseCells(cells: Actor[][]) {
+  const MIN_ERASE = 3
+  const eraseFlags: boolean[][] = []
+  for (let y = 0; y < cells.length; y++) {
+    eraseFlags[y] = []
+    for(let x = 0; x < cells[0].length; x++) {
+      eraseFlags[y][x] = false
+    }
+  }
+
+  // 横方向の並びをチェック
+  for (let y = 0; y < cells.length; y++) {
+    let cont = 1
+    let prevColor = cells[y][0].color.toHex()
+    for (let x = 1; x < cells[0].length; x++) {
+      if (cells[y][x].color.toHex() === prevColor) {
+        cont++
+        if (cont >= MIN_ERASE) {
+          for (let i = 0; i < cont; i++) {
+            eraseFlags[y][x-i] = true
+          }
+        }
+      } else {
+        cont = 1
+        prevColor = cells[y][x].color.toHex()
+      }
+    }
+  }
+  // 縦方向の並びをチェック
+  for (let x = 0; x < cells[0].length; x++) {
+    let cont = 1
+    let prevColor = cells[0][x].color.toHex()
+    for (let y = 1; y < cells.length; y++) {
+      if (cells[y][x].color.toHex() === prevColor) {
+        cont++
+        if (cont >= MIN_ERASE) {
+          for (let i = 0; i < cont; i++) {
+            eraseFlags[y-i][x] = true
+          }
+        }
+      } else {
+        cont = 1
+        prevColor = cells[y][x].color.toHex()
+      }
+    }
+  }
+
+  cells.forEach((row, y) => {
+    row.forEach((cell, x) => {
+      if (eraseFlags[y][x]) {
+        cells[y][x].kill()
+        cells[y][x] = undefined
+      }
+    })
+  })
+}
