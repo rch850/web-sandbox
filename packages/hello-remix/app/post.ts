02f95d95ffe8500ec5ea89d0c1088c8cbbff0594
rch850 <rich850@gmail.com>
2021-11-27 03:54:55 +0900

    👍 hello-remix: Dynamic Route Params

--- packages/hello-remix/app/post.ts
+++ packages/hello-remix/app/post.ts
@@ -2,6 +2,7 @@ import path from "path";
 import fs from"fs/promises";
 import parseFrontMatter from "front-matter";
 import invariant from "tiny-invariant";
+import { marked } from "marked";
 
 export type Post = {
   slug: string;
@@ -40,4 +41,18 @@ export async function getPosts() {
       }
     })
   );
+}
+
+export async function getPost(slug: string) {
+  let filepath = path.join(postsPath, slug + ".md");
+  let file = await fs.readFile(filepath);
+  let { attributes, body } = parseFrontMatter(
+    file.toString()
+  );
+  invariant(
+    isValidPostAttributes(attributes),
+    `Post ${filepath} is missing attributes`
+  );
+  let html = marked(body);
+  return { slug, html, title: attributes.title };
 }
