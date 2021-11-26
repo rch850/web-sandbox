a9d72d8ffa0ba835edf85ff46c251d5e9537e738
rch850 <rich850@gmail.com>
2021-11-27 03:30:03 +0900

    👍 hello-remix: Loading Data

--- packages/hello-remix/app/routes/posts/index.tsx
+++ packages/hello-remix/app/routes/posts/index.tsx
@@ -1,7 +1,37 @@
+import { Link, useLoaderData } from "remix";
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+export let loader = () => {
+  let posts: Post[] = [
+    {
+      slug: "my-first-post",
+      title: "My First Post"
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just For You"
+    }
+  ];
+  return posts;
+};
+
 export default function Posts() {
-    return (
-      <div>
-        <h1>Posts</h1>
-      </div>
-    );
-  }
+  let posts = useLoaderData<Post[]>();
+  console.log(posts);
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
