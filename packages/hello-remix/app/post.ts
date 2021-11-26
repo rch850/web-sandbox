916291761095b429deed8aeaa294c6dff614f271
rch850 <rich850@gmail.com>
2021-11-27 03:45:14 +0900

    👍 hello-remix: Pulling from a data source

--- packages/hello-remix/app/post.ts
+++ packages/hello-remix/app/post.ts
@@ -1,18 +1,43 @@
+import path from "path";
+import fs from"fs/promises";
+import parseFrontMatter from "front-matter";
+import invariant from "tiny-invariant";
+
 export type Post = {
   slug: string;
   title: string;
 };
 
-export function getPosts() {
-  let posts: Post[] = [
-    {
-      slug: "my-first-post",
-      title: "My First Post"
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You"
-    }
-  ];
-  return posts;
+export type PostMarkdownAttributes = {
+  title: string;
+};
+
+let postsPath = path.join(__dirname, "..", "posts");
+
+function isValidPostAttributes(
+  attributes: any
+): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
+
+export async function getPosts() {
+  let dir = await fs.readdir(postsPath);
+  return Promise.all(
+    dir.map(async filename => {
+      let file = await fs.readFile(
+        path.join(postsPath, filename)
+      );
+      let { attributes } = parseFrontMatter(
+        file.toString()
+      );
+      invariant(
+        isValidPostAttributes(attributes),
+        `${filename} has bad meta data!`
+      );
+      return {
+        slug: filename.replace(/\.md$/, ""),
+        title: attributes.title
+      }
+    })
+  );
 }
