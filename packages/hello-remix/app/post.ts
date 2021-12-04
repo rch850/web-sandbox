9b0b91d6020a49c27eccc3c80d912918b41ffd69
rch850 <rich850@gmail.com>
2021-12-04 09:48:48 +0900

    👍 hello-remix: Actions

--- packages/hello-remix/app/post.ts
+++ packages/hello-remix/app/post.ts
@@ -9,6 +9,12 @@ export type Post = {
   title: string;
 };
 
+type NewPost = {
+  title: string;
+  slug: string;
+  markdown: string;
+}
+
 export type PostMarkdownAttributes = {
   title: string;
 };
@@ -55,4 +61,13 @@ export async function getPost(slug: string) {
   );
   let html = marked(body);
   return { slug, html, title: attributes.title };
+}
+
+export async function createPost(post: NewPost) {
+  let md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
+  await fs.writeFile(
+    path.join(postsPath, post.slug + ".md"),
+    md
+  );
+  return getPost(post.slug)
 }
